a6da56c0503bd2f1e6eec696a930397fa4ec5180
jinia91 <creee77@gmail.com>
2025-02-23 15:11:18 +0900

    thread check

--- stomp_demo_simple_broker/src/main/kotlin/simplebroker/ChatMessageHandler.kt
+++ stomp_demo_simple_broker/src/main/kotlin/simplebroker/ChatMessageHandler.kt
@@ -5,12 +5,14 @@ import org.springframework.messaging.handler.annotation.SendTo
 import org.springframework.messaging.handler.annotation.DestinationVariable
 import org.springframework.stereotype.Controller
 
+private val log = mu.KotlinLogging.logger {}
+
 @Controller
 class ChatController {
-
     @MessageMapping("/chatroom/{roomId}")
     @SendTo("/topic/room/{roomId}")
     fun sendMessage(@DestinationVariable roomId: String, message: ChatMessage): ChatMessage {
+        log.info { "Received message: $message" }
         return message
     }
 }
